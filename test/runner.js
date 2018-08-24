c95aecd89f08e3e5997d559e29e60464ac18ce34
Ash Wilson <smashwilson@github.com>
2018-08-24 08:36:03 -0400

    Pass a terminate() function to the callback

--- test/runner.js
+++ test/runner.js
@@ -5,4 +5,9 @@ import {createRunner} from '../'
 module.exports = createRunner({
   globalAtom: false,
   htmlTitle: "atom-mocha-test-runner tests"
+}, (mocha, {terminate}) => {
+  if (process.env.ATOM_MOCHA_TERMINATE === 'true') {
+    console.log('Terminating test suite as requested.')
+    terminate()
+  }
 })
